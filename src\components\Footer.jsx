10c913979a3c432b593336d7b630d76fd49ee31e
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-11 20:43:09 +0500

    Add src\components\Footer.jsx

---
+++ src\components\Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-200 h-screen flex justify-center items-center">
+      <div className="container mx-auto p-4 flex justify-center items-center">
+        <p className="text-gray-600 text-center">
+          &copy; 2023 Pak Wheels. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
